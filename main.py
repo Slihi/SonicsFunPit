50994f563c0f4ff089f0b4ffdbd11c0b6196ce26
Slihi <jmacinty3021@gmail.com>
2023-10-29 22:55:19 -0400

    Putting in shader sources--still need to impliment shader class

--- main.py
+++ main.py
@@ -10,6 +10,36 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 TAN = (210, 180, 140)
 
+#Shader source code for pyglet'
+#Vertex shader
+vertex_source = """
+#version 150 core
+in vec2 position;
+in vec4 colors;
+out vec4 vertex_colors;
+
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * vec4(position, 0.0, 1.0);
+    vertex_colors = colors;
+}
+"""
+
+#Fragment shader
+fragment_source = """
+#version 150 core
+in vec4 vertex_colors;
+out vec4 final_color;
+
+void main()
+{
+    final_color = vertex_colors;
+}
+"""
+
+
 #Custom Window Class
 class Window(pyglet.window.Window):
     def __init__(self, minimum_size=(400, 300),
